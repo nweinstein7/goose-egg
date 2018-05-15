98f4074155cdb752cdadaceccedb6f6ffc3d6e67
Nathaniel Weinstein <nathaniel.weinstein@gmail.com>
2018-05-14 20:49:52 -0700

    build: add .travis.yml for ci/cd

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,12 +4,13 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-module TestCase
-  class ActiveSupport
-    # Setup all fixtures in test/fixtures/*.yml for
-    # all tests in alphabetical order.
-    fixtures :all
+# rubocop:disable ClassAndModuleChildren
+class ActiveSupport::TestCase
+  # rubocop:enable ClassAndModuleChildren
 
-    # Add more helper methods to be used by all tests here...
-  end
+  # Setup all fixtures in test/fixtures/*.yml for
+  # all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
 end
