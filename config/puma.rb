3aa010dbcbe8a93abb1b2a7fa48e6b1f26c6f726
Nathaniel Weinstein <nathaniel.weinstein@gmail.com>
2018-05-15 00:17:08 -0700

    fix: local development still needs to work

--- config/puma.rb
+++ config/puma.rb
@@ -36,5 +36,7 @@ preload_app!
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-bind 'unix:///var/run/puma/my_app.sock'
-pidfile '/var/run/puma/my_app.sock'
+if ENV.fetch('RAILS_ENV') == 'production'
+  bind 'unix:///var/run/puma/my_app.sock'
+  pidfile '/var/run/puma/my_app.sock'
+end
